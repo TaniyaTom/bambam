cadd2bf95abdbc07222721a49d6a26af3deb15c9
Administrator <Administrator@192.168.1.25>
2015-08-24 16:49:55 +0530

    First Commit

---
+++ FinanceUsingRESTServer/src/finance/ConnectionStatus.java
@@ -0,0 +1,23 @@
+package finance;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+public class ConnectionStatus {
+//lol
+@Path("/ConnectionStatus")
+public class TestClass {
+	@GET
+	@Path("/Change")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getUsers(){
+		System.out.println("Testing Rest");
+		String str = "{\"success\":true,\"code\":\"IN\",\"name\":\"INDIA hello\","+
+			      "\"continent\":\"ASIA cont\",\"region\":\"TamilNadu\",\"lifeExpectancy\":990.0,\"gnp\":98.0}";
+		return str;
+	}
+}
+
+}
